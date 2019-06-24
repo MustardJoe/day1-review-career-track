b5caeb35747ea07329d5e994b2ba211cbf171348
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-24 10:59:02 -0700

    excercise 1

---
+++ index.js
@@ -0,0 +1,21 @@
+// function createDog(name, age, weight) {
+//     return {
+//         name,
+//         age,
+//         weight,
+//     };
+// }
+
+// const dog = createDog('barney', 26, '300lbs');
+
+// console.log(dog);
+
+const createDog = (name, age, weight) => ({
+    name,
+    age, 
+    weight,
+});
+
+const dog1 = createDog('ralph', 10, '200lbs');
+
+console.log(dog1);
